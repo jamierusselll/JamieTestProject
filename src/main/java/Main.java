4b67e72ca65422a02a1a0680588e0bc6839abaa9
07144 <07144@farnborough.ac.uk>
2020-11-05 13:56:00 +0000

    yeh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,29 @@
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        String greeting = "hello, world!";
-        System.out.println(greeting);
-        System.out.println(greeting.length());
-//pog
+    static class Student {
+
+        public int studentNumber;
+        public String dob;
+        public String favSubject;
     }
+
+    public static void main(String[] args) {
+        Student PennyLane = new Student();
+        PennyLane.studentNumber = 5;
+        PennyLane.dob = "13/10/2005";
+        PennyLane.favSubject = "Computer Science";
+        Student AbbeyRoad = new Student();
+        AbbeyRoad.studentNumber = 6;
+        AbbeyRoad.dob = "14/10/2005";
+        AbbeyRoad.favSubject = "Computing";
+        ArrayList<Student> myStudents = new ArrayList<>();
+        myStudents.add(PennyLane);
+        myStudents.add(AbbeyRoad);
+        System.out.println(myStudents.get(0).dob);
+        for (Student s : myStudents) {
+            System.out.println(s.favSubject);
+        }
+    }
+
 }
